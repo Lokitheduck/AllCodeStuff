f97c9618e7195a720db7321b48cf01f6fd723996
Lokitheduck <Tigerlillytiggy@gmail.com>
2024-09-30 13:59:14 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            Console.WriteLine("Please input a number");
+            int NumberChoosen = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input another number");
+            int NumberChoosen1 = Convert.ToInt32(Console.ReadLine());
+
+
+            // hi love guithub
+            if (NumberChoosen == NumberChoosen1)
+            {
+                Console.WriteLine(NumberChoosen + "is equal to" + NumberChoosen1);
+
+            }
+            else
+            {
+                Console.WriteLine(NumberChoosen + "is not equal to " + NumberChoosen1);
+
+            }
+
+           
+            
         }
     }
 }
